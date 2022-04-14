e93c65becc45ac4aef29003dd41146b81416e2d0
Babita Bisht <84259086+Bbisht01@users.noreply.github.com>
2022-04-14 11:02:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Doctor from './components/Doctor';
+
+function App() {
+  return (
+    <div className="App">
+      
+     <Doctor/>
+    </div>
+  );
+}
+
+export default App;
