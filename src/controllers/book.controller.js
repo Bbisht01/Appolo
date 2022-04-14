8303cd76497055b3cd3e5e80b9f01dcc9525a1b4
Babita Bisht <84259086+Bbisht01@users.noreply.github.com>
2022-04-14 10:33:42 +0530

    Delete src directory

--- src/controllers/book.controller.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-const router = Router()
-const db = require("../configs/db.json")
-
-router.get("", (req, res) => {
-  res.status(200).json(db)
-})
-
-module.exports = router
